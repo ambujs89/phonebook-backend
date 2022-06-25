9dae58f076ea0f5707406d5aa17a6db77faf1ab7
ambujs89 <ambujs89@gmail.com>
2022-06-25 09:04:46 +0530

    get, post and delete added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+app.use(express.json())
+
+let persons = [
    { 
      "id": 1,
      "name": "Arto Hellas", 
@@ -24,3 +26,80 @@ const persons = [
    }
 ]
 
+app.get('/api/persons', (request, response) => {
+   response.json(persons)
+})
+
+app.get('/info', (request,response) => {
+   const date = new Date()
+   response.send(
+      `<div>The phonebook has ${persons.length} people</div>
+      <div>${date}</div>`
+   )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id)
+   const person = persons.find(person => person.id === id)
+
+   if(person) {
+      response.json(person)
+   } else {
+      response.status(404).end()
+   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+   const id = Number(request.params.id)
+   persons = persons.filter(person => person.id !== id)
+   
+   response.status(204).end()
+})
+
+const getRandomInt = (min, max) => {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const generateId = () => {
+   const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+
+   return getRandomInt(maxId + 1, 1000)
+}
+
+app.post('/api/persons', (request, response) => {
+   const body = request.body
+
+   if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+   }
+
+   const alreadyExists = persons.filter(person => person.name === body.name)
+
+   console.log(alreadyExists)
+
+   if(alreadyExists) {
+      return response.status(406).json({
+         error: 'persons already exists'
+      })
+   }
+
+   const person = {
+      id : generateId(),
+      name : body.name,
+      number : body.number,
+   }
+
+   persons = persons.concat(person)
+   response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
