41ecaef42ab7000f673b2d7ef5fc8477f9118aaf
ambujs89 <ambujs89@gmail.com>
2022-06-25 13:57:43 +0530

    remove morgan

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 morgan.token('body', (request, response) => {
    return JSON.stringify(request.body)
