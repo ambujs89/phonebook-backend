73b8343ae33bc69d9caa868e8209220c7b74c2bb
ambujs89 <ambujs89@gmail.com>
2022-06-28 09:45:31 +0530

    uibuild

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const errorHandler = (error, request, response, next) => {
    if (error.name === "CastError") {
       return response.status(400).send({ error: "malformatted id" })
    } else if (error.name === "ValidationError") {
-      return response.status(400).json({ error: error.message })
+      return response.status(406).json({ error: error.message })
    }
 
    next(error)
